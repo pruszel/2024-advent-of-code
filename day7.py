caac7081b00e9d2aad17397ad9fde467debc76b6
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2024-12-10 01:51:50 -0800

    Add solution for day 7 part 2

--- day7.py
+++ day7.py
@@ -2,41 +2,64 @@ from typing import Union
 
 
 class Equation:
-    def __init__(self, sum: int, values: list[int]):
+    def __init__(self, sum: int, operands: list[int]):
         self.sum = sum
-        self.values = values
+        self.operands = operands
 
-    def is_valid(self) -> bool:
-        operators: list[str] = ['+'] * (len(self.values) - 1)
-        if self.sum == self.calc_sum_with_operators(operators):
-            return True
-        for i in range(0, 2 ** len(operators)):
-            binary = format(i, f'0{len(operators)}b')
-            for j in range(0, len(operators)):
-                operators[j] = '*' if binary[j] == '1' else '+'
-            if self.sum == self.calc_sum_with_operators(operators):
+    def is_valid_with_operators(self, operators: list[str]) -> bool:
+        """Returns True if some possible combination of the given operators with the equation's operands equals the sum."""
+
+        def evaluate(operands: list[int], operators: list[str]) -> int:
+            """Calculates the sum of the operands with the given operators."""
+            sum = operands[0]
+            for i, operator in enumerate(operators[:len(operands) - 1]):
+                next_operand = operands[i + 1]
+                if operator == '+':
+                    sum += next_operand
+                elif operator == '*':
+                    sum *= next_operand
+                elif operator == '||':
+                    sum = int(str(sum) + str(next_operand))
+            return sum
+
+        def generate_operator_permutations(operators: list[str], current_operators: list[str] = []) -> list[list[str]]:
+            """
+            Generates all possible permutations of the given operators using recursion.
+
+            Example:
+            ['+'],
+            ['+', '+'],
+            ['+', '*'],
+            ['*'],
+            ['*', '+'],
+            ['*', '*']
+            """
+            if len(current_operators) == len(self.operands) - 1:
+                # got them all
+                return [current_operators]
+            permutations = []
+            for operator in operators:
+                permutations.extend(generate_operator_permutations(
+                    operators, current_operators + [operator]))
+            return permutations
+
+        permutations = generate_operator_permutations(operators)
+        for permutation in permutations:
+            if evaluate(self.operands, permutation) == self.sum:
                 return True
-        return False
 
-    def calc_sum_with_operators(self, operators: list[str]) -> int:
-        sum = self.values[0]
-        for i in range(1, len(self.values)):
-            if operators[i - 1] == '+':
-                sum += self.values[i]
-            elif operators[i - 1] == '*':
-                sum *= self.values[i]
-        return sum
+        return False
 
     @staticmethod
     def from_string(string: str) -> Union['Equation', None]:
         if not string:
             return None
         sum = int(string.split(':')[0])
-        values = list(map(int, string.split(':')[1].split(' ')[1:]))
-        return Equation(sum, values)
+        operands = list(map(int, string.split(':')[1].split(' ')[1:]))
+        return Equation(sum, operands)
 
     def __repr__(self) -> str:
-        return f"{self.sum} = {self.values.join(' ? ')}"
+        return f"{self.sum} = {self.operands.join(' ? ')}"
 
 
 class Day7:
@@ -48,18 +71,27 @@ class Day7:
     def part1_answer(self) -> int:
         answer: int = 0
         for equation in self.equations:
-            if equation.is_valid():
-                answer += equation.sum
+            answer += equation.sum \
+                if equation.is_valid_with_operators(['+', '*']) else 0
+        return answer
+
+    def part2_answer(self) -> int:
+        answer: int = 0
+        for equation in self.equations:
+            answer += equation.sum if \
+                equation.is_valid_with_operators(['+', '*', '||']) else 0
         return answer
 
     def read_equations_file(self) -> list[Equation]:
         equations = []
         with open(self.input_file_path) as file:
             for line in file:
-                equations.append(Equation.from_string(line.strip()))
+                if line.strip():
+                    equations.append(Equation.from_string(line.strip()))
         return equations
 
 
 if __name__ == '__main__':
     day7 = Day7('day7-input.txt')
     print(f"Part 1 answer: {day7.part1_answer()}")
+    print(f"Part 2 answer: {day7.part2_answer()}")
